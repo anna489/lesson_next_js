3b3c2fcf311ee342d2b0327e07699d2caf2849ba
anar <LP230138@G301-CC0172.local>
2023-10-02 18:25:39 +0800

    edit

--- lesson_02_use_effect/pages/Footer.jsx
+++ lesson_02_use_effect/pages/Footer.jsx
@@ -1,7 +1,61 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="container mx-auto p-8 bg-slate-100">
+      <div class="grid grid-cols-3 justify-between gap-20  ">
+        <div className=" gap-5 grid">
+          <h1>About</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam
+          </p>
+          <div>
+            <ul>
+              <li>Email : info@jstemplate.net</li>
+              <li>Phone : 880123456789</li>
+            </ul>
+          </div>
+        </div>
+        <div class="...">
+          <ul>
+            <li>Home</li>
+            <li>Blog</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div class="...">
+          <ul className="flex gap-10">
+            <li>
+              <img src="./Negative.png" />
+            </li>
+            <li>
+              <img src="./twitter.png" />
+            </li>
+            <li>
+              <img src="./ig.png" />
+            </li>
+            <li>
+              <img src="./link.png" />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="justify-between flex">
+        <div>
+          <img src="./Logo (2).png" />
+        </div>
+        <div>
+          <ul class="flex gap-10">
+            <li>Terms of Use </li>
+            <li>Privacy Policy</li>
+            <li>Cookie Policy</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- lesson_02_use_effect/pages/RecentBlog.jsx
+++ lesson_02_use_effect/pages/RecentBlog.jsx
@@ -1,55 +1,71 @@
 import React from "react";
+import Link from "next/link";
 
 const RecentBlog = ({ blogs }) => {
   return (
     <div className={`container mx-auto p-8 `}>
       <div>
         <h1 class="p-5 text-2xl font-normal">Recent Blog posts</h1>
-        <div class="grid grid-rows-3 grid-flow-col gap-4 ">
-          <div class="row-span-3 bg-slate-300 border ">
-            <div className="">
-              <img src={blogs[0]?.cover_image} />
+        <div class="flex gap-5  ">
+          <div class="row-span-3 bg-slate-300">
+            <div className="1 ">
+              <img src={blogs[0]?.cover_image} className="h-50" />
               <p>{blogs[0]?.readable_publish_date}</p>
+              <h1>{blogs[0]?.title}</h1>
+              <p>{blogs[0]?.description}</p>
             </div>
           </div>
-          <div class="col-span-2 bg-slate-500">
-            <div>
-              <img src={blogs[0]?.cover_image} />
-              <p>{blogs[0]?.readable_publish_date}</p>
-            </div>
-          </div>
-          <div class="row-span-2 col-span-2 bg-slate-600">
-            <div>
-              <img src={blogs[0]?.cover_image} />
-              <p>{blogs[0]?.readable_publish_date}</p>
+          <div className="2">
+            <div style={{ display: "flex" }}>
+              <div>
+                <img src={blogs[0]?.cover_image} />
+              </div>
+              <div>
+                <p>{blogs[0]?.readable_publish_date}</p>
+                <h1>{blogs[0]?.title}</h1>
+                <p>{blogs[0]?.description}</p>
+              </div>
+              <div />
+
+              <div>
+                <div>
+                  <img src={blogs[0]?.cover_image} />
+                </div>
+                <p>{blogs[0]?.readable_publish_date}</p>
+                <h1>{blogs[0]?.title}</h1>
+                <p>{blogs[0]?.description}</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
       <div>
         <h1 class="p-5 text-2xl font-medium">All Blog Posts</h1>
       </div>
       <div className="grid grid-cols-3">
         {blogs.map((blog, i) => (
-          <div className="border felx-col p-2 rounded m-2   shadow-lg  ">
-            <img src={blog.cover_image} className="rounded" />
-            <div className="flex flex-col">
-              <div className="text-blue-700 bg-gray-100 rounded p-1 m-3">
-                <span>{blog?.type_of}</span>
+          <Link href={"/blog/" + blog.id}>
+            <div className="border felx-col p-2 rounded m-2   shadow-lg  ">
+              <img src={blog.cover_image} className="rounded" />
+              <div className="flex flex-col">
+                <div className="text-blue-700 bg-gray-100 rounded p-1 m-3">
+                  <span>{blog?.type_of}</span>
+                </div>
+                <h2>{blog.title}</h2>
               </div>
-              <h2>{blog.title}</h2>
+              <div className="flex items-center gap-2 text-gray-500 text justify-around">
+                <img
+                  src={blog.user.profile_image}
+                  className="rounded-full"
+                  height={50}
+                  width={50}
+                />
+                <p>{blog.user.name}</p>
+                <p>{blog.readable_publish_date}</p>
+              </div>
             </div>
-            <div className="flex items-center gap-2 text-gray-500 text justify-around">
-              <img
-                src={blog.user.profile_image}
-                className="rounded-full"
-                height={50}
-                width={50}
-              />
-              <p>{blog.user.name}</p>
-              <p>{blog.readable_publish_date}</p>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
